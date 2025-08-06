a5beb1c36fdd2ff2241757dd6ea81119f1adf0ad
Maria Geovana Gonçalo de Oliveira <mariageovana82268@gmail.com>
2025-08-06 17:57:39 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var mensagem = document.querySelector('#mensagem')
+    var imagem = document.querySelector('#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    mensagem.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
+    if (hora >= 6 && hora < 12) {
+        document.body.style.background = '#657092'
+        imagem.src = 'images/manhã.jpg'
+    } else if (hora >=12 && hora < 18) {
+        document.body.style.background = '#946265'
+        imagem.src = 'images/tarde.jpg'
+    } else {
+        document.body.style.background = '#101829'
+        imagem.src = 'images/noite.jpg'
+    }
+}
